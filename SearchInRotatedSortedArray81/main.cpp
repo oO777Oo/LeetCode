a506faca5d7bffdad1076095283bbb53ea8ecd48
Vladislav <vladislav.popovici96@gmail.com>
2021-02-04 15:29:45 +0100

    resolve problem 540 and some changes in problem 81

--- SearchInRotatedSortedArray81/main.cpp
+++ SearchInRotatedSortedArray81/main.cpp
@@ -4,33 +4,64 @@
 using namespace std;
 
 bool search(vector<int>& nums, int target) {
-    long long int start = 0;
-    long long int end = nums.size() - 1;
+    long int start = 0;
+    long long int end = nums.size();
+
+    if(nums.size() == 1) {
+        return nums[0] == target;
+    }
+    if (nums.size() == 2) {
+        return target == nums[0] || target == nums[1];
+    }
 
     while (start <= end) {
-        long long int mid = (start + end) / 2;
+        int mid = (end + start) / 2;
+        int left = mid;
+        int right = mid;
         if (nums[mid] == target) {
             return true;
         }
-        if (nums[start] <= nums[mid]) {
-            if (target > nums[start] && target < nums[mid]) {
+        if(mid != 0 && mid != nums.size()-1 && (nums[mid] == nums[mid -1] || nums[mid] == nums[mid + 1])) {
+            while (left >= start && right <= end) {
+                if (nums[left] != nums[mid]) {
+                    mid = left;
+                    break;
+                } else {
+                    left--;
+                }
+                if (nums[right] != nums[mid]) {
+                    mid = right;
+                    break;
+                } else {
+                    right++;
+                }
+            }
+        }
+        if (nums[mid] == target) {
+            return true;
+        }
+        if (nums[start] < nums[mid]) {
+            if (target <= nums[mid] && target >= nums[start]) {
                end = mid - 1;
             } else {
                 start = mid + 1;
             }
         } else {
-            if (target > nums[mid] && target < nums[end]) {
-                start = mid + 1;
-            } else {
-                end = mid - 1;
-            }
+           if(target >= nums[end - 1] && nums[mid] >= target) {
+               start = mid + 1;
+           } else {
+               end = mid - 1;
+           }
         }
+
     }
     return false;
 }
 
+
 int main() {
-    vector<int> x = {1,0,1,1,1}; // fail on this test case
-    std::cout << search(x, 0) << std::endl;
+    //[1,1,1,1,1,1,1,1,1,13,1,1,1,1,1,1,1,1,1,1,1,1]
+    vector<int> x = {2,5,6,0,0,1,2}; // fail on this test case
+    std::cout << search(x, 3) << std::endl;
     return 0;
 }
--- SingleElementInASortedArray540/main.cpp
+++ SingleElementInASortedArray540/main.cpp
@@ -14,45 +14,35 @@ int singleNonDuplicate1(vector<int>& nums) {
     }
 }
 
+/**
+ * Using binary search
+ * */
 int singleNonDuplicate(vector<int>& nums) {
     int start = 0;
-    int end = nums.size() - 1;
-    if(end == 0) {
+    int end = nums.size();
+    if (nums.size() == 1) {
         return nums[0];
     }
-    if (nums[0] != nums[1]) {
-        return nums[0];
-    }
-    if (nums[end] != nums[end - 1]) {
-        return nums[end];
-    }
-
-    while (start <= end) {
-        int mid = (start + end) / 2;
-        if(nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1]) {
+    while (end - start > 1) {
+        int mid = (end + start) / 2;
+        if (nums[mid] == nums[mid + 1]) {
+            mid++;
+        } else if (nums[mid] != nums[mid - 1]) {
             return nums[mid];
         }
-        if (nums[mid] == nums[mid + 1]) {
-            if((end - mid + 1) % 2 == 0) {
-                end = mid;
-            } else if ((end - mid + 1) % 2 != 0) {
-                start = mid - 1;
-            }
-        } else if (nums[mid] == nums[mid - 1]) {
-            if((mid - 1 - start) % 2 == 0){
-                start = mid;
-            }else if ((mid - 1 - start) % 2 != 0) {
-                end = mid - 1;
-            }
+        if ((end - 1 - mid) % 2 == 0) {
+            end = mid - 1;
+        } else {
+            start = mid + 1;
         }
     }
-    return 0;
+    return nums[start];
 }
 
 int main() {
     // [1,1,2,3,3,4,4,8,8]
     // [0,1,2,3,4,5,6,7,8]
-    vector<int> nums = {2,2,3,3,4,4,8};
+    vector<int> nums = {3,3,7,7,10,11,11};
     int x = singleNonDuplicate(nums);
     std::cout << x << std::endl;
 
