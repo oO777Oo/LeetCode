6a83cc105c6d7db3b863d4116200c79fc9918f46
Vladislav <vladislav.popovici96@gmail.com>
2021-02-22 22:15:39 +0100

    solved problem with number 3 using c++

--- LongestSubstringWithoutRepeatingCharacters3/main.cpp
+++ LongestSubstringWithoutRepeatingCharacters3/main.cpp
@@ -11,7 +11,7 @@ int main() {
 }
 
 int lengthOfLongestSubstring(string s) {
-    if(s.size() == 0) {
+    if (s.size() == 0) {
         return 0;
     }
     int arr[128] = {};
@@ -20,25 +20,18 @@ int lengthOfLongestSubstring(string s) {
     int currentLength = 0;
     int pointer = 0;
 
-    for(int i = 0; i < s.size(); i++) {
-        if(arr[s[i]] == 0) {
-            currentLength++;
-            arr[s[i]] ++;
-            if (currentLength > maxLength) {
-                maxLength = currentLength;
-            }
-        } else {
-            if (s[pointer] == s[i]) {
-               pointer++;
-            } else {
-                while (s[pointer] != s[i]) {
-                    arr[s[pointer]]--;
-                    pointer++;
-                }
-            }
-            currentLength = i - pointer;
+    for (int i = 0; i < s.size(); i++) {
+        arr[s[i]]++;
+        currentLength++;
+
+        while (arr[s[i]] > 1) {
+            arr[s[pointer]]--;
+            pointer++;
+            currentLength--;
+        }
+        if (currentLength > maxLength) {
+            maxLength = currentLength;
         }
     }
-
     return maxLength;
 }
