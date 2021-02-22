8a875bf9db23dd3dc55913caca888f3d146a238c
Vladislav <vladislav.popovici96@gmail.com>
2021-02-22 05:24:38 +0100

    Solved problem 713

--- SubarrayProductLessThanK713/main.cpp
+++ SubarrayProductLessThanK713/main.cpp
@@ -5,49 +5,27 @@ using namespace std;
 
 int numSubarrayProductLessThanK(vector<int> &nums, int k);
 
-/*TODO check for test case
- * [10,2,2,5,4,4,4,3,7,7]
- * 289
- * Need some changes
- * */
-
-
 int main() {
-
-    /**
-     * [10,9,10,4,3,8,3,3,6,2,10,10,9,3]
-        19
-     *
-     * */
-    vector<int> nums = {1, 1, 1};
-    int k = 2;
+    vector<int> nums = {10, 9, 10, 4, 3, 8, 3, 3, 6, 2, 10, 10, 9, 3};
+    int k = 19;
 
     cout << numSubarrayProductLessThanK(nums, k) << endl;
     return 0;
 }
 
 int numSubarrayProductLessThanK(vector<int> &nums, int k) {
-    int pointer1 = 0;
-    int pointer2 = 0;
-    int product = 1;
-    int ans = 0;
+    if (k <= 1) {
+        return 0;
+    }
+    int pointer1 = 0, pointer2 = 0, ans = 0, product = 1;
 
-    while (pointer1 < nums.size()) {
-        if (pointer2 != nums.size() && product * nums[pointer2] < k) {
-            product *= nums[pointer2];
-            pointer2++;
-            ans++;
-            if (nums.size() == pointer2 && pointer1 + 1 == pointer2) {
-                break;
-            }
-        } else if (product < k && (pointer2 - pointer1 > 1 || pointer2 == nums.size())) {
-            product /= nums[pointer1];
-            pointer1++;
-            ans++;
-        } else {
-            product /= nums[pointer1];
-            pointer1++;
+    while (pointer2 < nums.size()) {
+        product *= nums[pointer2];
+        while (product >= k) {
+            product /= nums[pointer1++];
         }
+        ans += pointer2 - pointer1 + 1;
+        pointer2++;
     }
     return ans;
 }
