527b751429bc9588a5b6c64fab531f5a299472d9
Vladislav <vladislav.popovici96@gmail.com>
2021-02-26 11:52:47 +0100

    add solution from problem 1598

--- Easy/CrawlerLogFolder1598/main.cpp
+++ Easy/CrawlerLogFolder1598/main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
+#include <vector>
+
+using namespace std;
+
+int minOperations(vector<string>& logs);
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
+}
+
+int minOperations(vector<string>& logs) {
+    vector<string> ans = {};
+    for (int i= 0; i < logs.size(); i++) {
+        if (logs[i] == "../") {
+            if(!ans.empty()) {
+                ans.pop_back();
+            }
+        } else if (logs[i] == "./") {
+            continue;
+        } else {
+            ans.push_back(logs[i]);
+        }
+    }
+    return ans.size();
 }
