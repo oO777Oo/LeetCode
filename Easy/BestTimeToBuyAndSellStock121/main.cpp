01eae752b93e14e55ca5c75a547772d19505b361
Vladislav <57865785+oO777Oo@users.noreply.github.com>
2021-03-12 14:27:09 +0100

    Update main.cpp
    reformat file

--- Easy/BestTimeToBuyAndSellStock121/main.cpp
+++ Easy/BestTimeToBuyAndSellStock121/main.cpp
@@ -4,14 +4,14 @@
 using namespace	std;
 
 int maxProfit(vector<int>& prices) {
-	int min = prices[0];
-	int max = 0;
+    int min = prices[0];
+    int max = 0;
 	for (int i = 1; i < prices.size(); i++) {
-		if (min > prices[i]) {
-			min = prices[i];
-		}else if(max < prices[i] - min) {
-			max = prices[i] - min;
-		}
+	    if (min > prices[i]) {
+		min = prices[i];
+            } else if(max < prices[i] - min) {
+		max = prices[i] - min;
+	    }
 	}        
 	return max;
 }
